52b2c02ead1350216fd7a66966e835ec11f30a5a
Zyrakia <ole.lammers@pm.me>
2024-06-25 12:10:27 -0700

    initial commit

---
+++ src/main/java/dev/zyrakia/Neuw.java
@@ -0,0 +1,10 @@
+package dev.zyrakia;
+
+/**
+ * Neuw is a dynamic and easy to use project bootstrapper.
+ *
+ * @author Zyrakia
+ */
+public class Neuw {
+	public static void main(String[] args) { }
+}
