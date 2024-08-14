4b4624155774006d2e16bfd21c581eeffed64829
Zyrakia <ole.lammers@pm.me>
2024-08-14 12:05:08 -0700

    feat: added process runner package to support command execution

---
+++ src/main/java/dev/zyrakia/neuw/app/pkg/ProcessRunnerPackage.java
@@ -0,0 +1,71 @@
+package dev.zyrakia.neuw.app.pkg;
+
+import static org.jline.jansi.Ansi.ansi;
+
+import java.io.IOException;
+
+import org.jline.jansi.Ansi.Color;
+
+import dev.zyrakia.neuw.app.TerminalApp;
+import dev.zyrakia.neuw.cmd.ProcessRunner;
+
+/**
+ * This package runs the given process and prompts the user before continuing.
+ * All output (standard or error) will be piped into the terminal app the
+ * package is run on.
+ */
+public class ProcessRunnerPackage implements TerminalPackage<Integer> {
+
+    /**
+     * The process builder used to launch processes when this package is run.
+     */
+    private final ProcessBuilder pb;
+
+    /**
+     * Creates a new package with the given process builder.
+     * 
+     * @param pb the process builder to launch processes with
+     */
+    public ProcessRunnerPackage(ProcessBuilder pb) {
+        this.pb = pb;
+    }
+
+    @Override
+    public Integer execute(TerminalApp app) {
+        try {
+            ProcessRunner runner = new ProcessRunner(this.pb, (
+                    v) -> pipeStandard(app, v), (v) -> pipeError(app, v));
+
+            int exit = runner.waitFor();
+
+            app.newLine();
+            app.run(new ContinuePromptPackage());
+
+            return exit;
+        } catch (IOException | InterruptedException e) {
+            app.writeException(e);
+            return 1;
+        }
+    }
+
+    /**
+     * Pipes the given line into the given app as standard output.
+     * 
+     * @param app the app to pipe to
+     * @param line the line to pipe
+     */
+    private static void pipeStandard(TerminalApp app, String line) {
+        app.writer().println(line);
+    }
+
+    /**
+     * Pipes the given line into the given app as error highlighted output.
+     * 
+     * @param app the app to pipe to
+     * @param line the line to pipe
+     */
+    private static void pipeError(TerminalApp app, String line) {
+        app.writer().println(ansi().fg(Color.RED).a(line).reset());
+    }
+
+}
